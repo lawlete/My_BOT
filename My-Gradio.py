4b9855662d1dce8e8bded3a5e8a99b9a33edc6de
Lawlete <alfredolawler@gmail.com>
2023-11-11 22:35:04 -0300

    Mix Gradio con Bot ChatGPT

--- My-Gradio.py
+++ My-Gradio.py
@@ -1,14 +1,31 @@
 import gradio as gr
 from distutils.version import StrictVersion
+import openai
+from config import API_KEY_OPENAI_1
+
+openai.api_key = API_KEY_OPENAI_1 # cargamos la API_KEY
 
 
-def saludo(nombre):
-    return "Te amo " + nombre + ", ¿Y vos??? "
+def raices(ecuacion):
+    respuesta = openai.completions.create( ## creamos una instancia de Consulta de Competion
+            model="text-davinci-003", # modelo a usar "gpt-3.5-turbo-instruct" "text-davinci-003"
+            prompt="Calcular las raices de la ecuacion: " + ecuacion # consulta o prompt
+            max_tokens=256 # cantidad maxima de tokens
+        )
+    return respuesta.choices[0].text.strip()
+   
+
+
+def ChatGPT(ecuacion):
+    return "La ecuacion " + ecuacion 
+            + "tiene las siguientes raices" 
+            + raices(ecuacion)
 
 
 demo = gr.Interface(
-    fn=saludo,
-    inputs = gr.components.Textbox(lines=10, placeholder="Dime tu nombre porfa"),
+    fn=chatGPT,
+    inputs = gr.components.Textbox(lines=10, 
+                                   placeholder="Escribe la ecuacion para cacular las raices "),
     outputs = "text"
 )
 
