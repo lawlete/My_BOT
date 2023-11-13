8ee07550607e5a9fea8534c14d3b61e62666fc03
Lawlete <alfredolawler@gmail.com>
2023-11-12 22:03:11 -0300

    Correccion de errores de My Gradio

--- My-Gradio.py
+++ My-Gradio.py
@@ -5,34 +5,39 @@ from config import API_KEY_OPENAI_1
 
 openai.api_key = API_KEY_OPENAI_1 # cargamos la API_KEY
 
-
 def raices(ecuacion):
-    mensaje=""" "Calcular las raices, la derivada, la integral, 
-            el limite cuando x tiende a 0, 
-            y cuando x tiende a oo de la siguiente ecuacion: " 
-            + ecuacion
-            """
+    mensaje= "Para la ecuacion: " 
+    mensaje= mensaje + str(ecuacion)
+    mensaje= mensaje + ", calcular las raices, la derivada, la integral "
+    mensaje= mensaje + "y el limite cuando x tiende a 0 y a infinito"
+    
+    #print(mensaje)
+    #print(" la Ecuacion es: ", str(ecuacion))
     respuesta = openai.completions.create( ## creamos una instancia de Consulta de Competion
             model="text-davinci-003", # modelo a usar "gpt-3.5-turbo-instruct" "text-davinci-003"
             prompt= mensaje, # consulta o prompt
-            max_tokens=256 # cantidad maxima de tokens
+            max_tokens=256, # cantidad maxima de tokens
+            temperature=0.1
         )
     return respuesta.choices[0].text.strip()
    
 
 
-def chatGPT(ecuacion):
-    return raices(ecuacion)
+#def chatGPT(ecuacion):
+#    return raices(ecuacion)
 
 
 demo = gr.Interface(
-    fn=chatGPT,
+    fn=raices,
     inputs = gr.components.Textbox(
-                lines=10, 
-                placeholder=""" "Escribe la ecuacion para calcular
+                lines=1,
+                max_lines=3, 
+                placeholder=""" 
+                "Escribe la ecuacion para calcular
                 las raices, la derivada, la integral,
                 los limites de x cuando tiende a 0,
-                y cuando x tiende a oo" """),
+                y cuando x tiende a oo" 
+                """),
     outputs = "text"
 )
 
